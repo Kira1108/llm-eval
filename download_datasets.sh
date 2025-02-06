adfc2575b430f0949d5cef392fc884581da9f082
王欢 <wanghuanr@yxqiche.com>
2025-02-06 13:46:24 +0800

    Initial commit

--- download_datasets.sh
+++ download_datasets.sh
@@ -3,3 +3,4 @@ mkdir -p data
 export HF_ENDPOINT=https://hf-mirror.com
 huggingface-cli download cais/mmlu --local-dir data/mmlu --repo-type dataset
 huggingface-cli download TIGER-Lab/MMLU-Pro --local-dir data/mmlu-pro --repo-type dataset
+huggingface-cli download openai/gsm8k --local-dir data/gsm8k --repo-type dataset
